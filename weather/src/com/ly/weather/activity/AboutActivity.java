ea8676743e89e4e802e187f48a6174ed922be7c6
peiniwan <410093000@qq.com>
2015-10-29 09:24:03 +0800

    加入更多

--- weather/src/com/ly/weather/activity/AboutActivity.java
+++ weather/src/com/ly/weather/activity/AboutActivity.java
@@ -1,14 +1,13 @@
 package com.ly.weather.activity;
 
-import com.ly.weather.R;
-
 import android.os.Bundle;
 import android.view.Window;
+
+import com.ly.weather.R;
 
 public class AboutActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.about_activity);
